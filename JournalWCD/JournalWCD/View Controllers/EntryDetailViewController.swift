aee5d6c4a5bc4fc5c84f56b0a462308d3bde81de
Michael R.F <mrflowers22@uwalumni.com>
2019-10-02 21:02:42 -0600

    implemented the table view data source methods and the segue to pass information from the cell

--- JournalWCD/JournalWCD/View Controllers/EntryDetailViewController.swift
+++ JournalWCD/JournalWCD/View Controllers/EntryDetailViewController.swift
@@ -10,25 +10,20 @@ import UIKit
 
 class EntryDetailViewController: UIViewController {
 
+    var entry: Entry?
+    var entryController: EntryController?
+    
     @IBOutlet weak var titleTextField: UITextField!
     @IBOutlet weak var bodyTextView: UITextView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
     }
     
     @IBAction func saveButtonPressed(_ sender: UIBarButtonItem) {
     }
     
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
+    
 
 }
